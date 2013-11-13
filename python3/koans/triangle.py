b508f18f8353f59a439200fc767ab6f7a3e656b2
tacothursday <dwang@ticklebutt.com>
2013-11-12 20:37:02 -0800

    finished: about_triangle_project next: about_exceptions

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,11 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a==b==c:
+        return 'equilateral'
+    elif len(set([a, b, c])) == 2:
+        return 'isosceles'
+    else: return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
